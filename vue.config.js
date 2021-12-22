d7f87cea7fd9bea3d14750069a05e9424136e5cf
Azuma Hiroki <ornitho1027@gmail.com>
2021-12-22 09:51:42 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // publicPath: process.env.NODE_ENV === 'production'
-    publicPath: "/",
-    outputDir: "docs"
+    outputDir: 'docs',
+    assetsDir: './',
+    publicPath: './',
   }
